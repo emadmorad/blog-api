b96f0c2a4171b7f6cfcfe6c921ff7c548a1aec9e
Emad Morad <emadmoradd@gmail.com>
2026-03-12 23:34:03 +0200

    Fix server.js middleware order

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const mongoose = require('mongoose');
 const postRoutes = require('./routes/postRoutes');
 const userRoutes = require('./routes/userRoutes');
 const errorHandler = require('./middleware/errorHandler');
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-
-app.use('/api/posts', postRoutes);
-app.use('/api/users', userRoutes);
-app.use(errorHandler);
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -26,6 +23,11 @@ mongoose.connect(process.env.MONGO_URI, {
 app.get('/', function(req, res) {
   res.json({ message: 'Blog API is working!' });
 });
+
+app.use('/api/posts', postRoutes);
+app.use('/api/users', userRoutes);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
