7a9c191913a5964f9fd65ed1bd512ebe1e11a282
Emad Morad <emadmoradd@gmail.com>
2026-03-14 07:02:37 +0200

    Fix Authorization header to use Bearer token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const auth = function(req, res, next) {
-  const token = req.header('Authorization');
+  const token = req.header('Authorization').replace('Bearer ', '');
 
   if (!token) {
     return res.status(401).json({ message: 'No token, access denied' });
